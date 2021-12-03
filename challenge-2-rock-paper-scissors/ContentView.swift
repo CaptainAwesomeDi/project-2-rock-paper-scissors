8c2478afa28745a4b3615ebdb72ace3384676e0c
Di Wu <di.wu@productboard.com>
2021-12-03 11:04:42 -0800

    Initial Commit

---
+++ challenge-2-rock-paper-scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge-2-rock-paper-scissors
+//
+//  Created by Di Wu on 2021-12-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challenge-2-rock-paper-scissors/challenge_2_rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_2_rock_paper_scissorsApp.swift
+//  challenge-2-rock-paper-scissors
+//
+//  Created by Di Wu on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct challenge_2_rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
